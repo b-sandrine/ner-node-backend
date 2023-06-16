ada2d5ece4f3107c9fd19bb38f22e48414096dc6
b-sandrine <sanrdushimimana@gmail.com>
2023-06-16 21:48:02 +0200

    Added user validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,30 @@
-const User = require('../models/user.model')
+const { User, validUser } = require('../models/user.model')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const KEY = process.env.SECRET_KEY;
+
 const addUser = async (req, res) => {
     try {
-        const user = new User(req.body);
-        const result = await user.save();
-        res.status(201).send(result)
+        const userData = req.body;
+        const {error} = validUser.validate(userData)
+
+        if (error) {
+            return res.status(400).json({error: error.details[0].message})
+        }
+        else {
+            const userExist = await User.findOne({email: userData.email})
+
+            if(userExist) {
+                return res.status(400).json({
+                    message: "User already exists",
+                    user: userExist
+                })
+            }
+            const user = new User(userData);
+            const result = await user.save();
+            res.status(201).json(result)
+        }
     }
     catch (err) {
         console.log(err)
@@ -16,30 +33,30 @@ const addUser = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    try{
+    try {
         const email = req.body.email;
-        const result = await User.findOne({email: email})
-        
+        const result = await User.findOne({ email: email })
+
         if (!result) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        const token = jwt.sign({id: result._id},KEY, {expiresIn: '6h'})
+        const token = jwt.sign({ id: result._id }, KEY, { expiresIn: '6h' })
 
         const payload = {
             user: result,
             token: token
         }
 
-        res.status(200).json({payload})
+        res.status(200).json({ payload })
     }
-    catch(err) {
+    catch (err) {
         console.log(err)
         res.status(400).send("User not found")
     }
 }
 
-const welcome = (req,res) => {
+const welcome = (req, res) => {
     res.status(200).send("Protected route")
 }
 module.exports = { addUser, login, welcome }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Joi = require('joi')
 
 const userSchema = mongoose.Schema({
     fullnames: {
@@ -26,4 +27,15 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model("users", userSchema);
 
-module.exports = User;
+const validUser = Joi.object({
+    fullnames: Joi.string().min(5).max(50).required(),
+    email: Joi.string().email().required(),
+    NID: Joi.number().integer().min(1190000000000000).max(1200700000000000).required(),
+    phoneNumber: Joi.string().required(),
+    address: Joi.string().required(),
+    username: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+})
+
+
+module.exports = {User, validUser};
